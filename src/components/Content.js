2305228d8bb45b2d4b066224106e83cdb6f5d581
SmallPeenwow <bowdencamryn@gmail.com>
2022-05-03 15:46:11 +0200

    Cover Page

---
+++ src/components/Content.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import ImageOne from '../images/Evel_Knievel-Chase.png'
+import ImageTwo from '../images/SupportKing.png'
+
+const Content = () => {
+  return (
+    <>
+        <div className="menu-card">
+            <img src={ImageOne} alt="" className="h-full rounded mb-20 shadow"/>
+            <div className="center-content">
+                <h2 className="text-2xl mb-2">Peen Memes SABUS</h2>
+                <p className="mb-2">Stunts where never the same after this man</p>
+                <span>R4500</span>
+            </div>
+        </div>
+        <div className="menu-card">
+            <img src={ImageTwo} alt="" className="h-full rounded mb-20 shadow"/>
+            <div className="center-content">
+                <h2 className="text-2xl mb-2">Support King</h2>
+                <p className="mb-2">Support never failed under this man</p>
+                <span>R44500</span>
+            </div>
+        </div>
+    </>
+  )
+}
+
+export default Content
---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className="flex justify-center items-center h-16 bg-black text-white">
+        <p>Copyright @ 2022 PEEN All rights matter</p>
+    </div>
+  )
+}
+
+export default Footer
