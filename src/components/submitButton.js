35ab854e921db6c83c93d552483879a6cde2a4a1
SmallPeenwow <bowdencamryn@gmail.com>
2022-05-20 23:36:09 +0200

    sumbit and phone number check value

--- src/components/submitButton.js
+++ src/components/submitButton.js
@@ -1,10 +1,19 @@
 import React from 'react'
 
-const SubmitButton = () => {
+const SubmitButton = (number, name, active) => {
+    const sendEmail = () => {
+        console.log(active)
+        if(active) {
+            console.log('fail')
+        } else {
+            console.log('works');
+        }
+    }
+
   return (
-    <div>
-        <button></button>
-    </div>
+    <>
+        <button className="order-button-purchase" onClick={sendEmail}>Order</button>
+    </>
   )
 }
 
--- src/pages/purchasePage.js
+++ src/pages/purchasePage.js
@@ -1,15 +1,41 @@
-import React from 'react'
+import React, { useState } from 'react'
 import SubmitButton from '../components/submitButton'
 
 const PurchasePage = () => {
+
+  const [isPhoneNumber, setIsPhoneNumber] = useState(0);
+
+  const [isName, setIsName] = useState("");
+
+  const [isError, setIsError] = useState(false);
+
+  const setNumber = (event) => {
+    if(event.target.value.length > 11 || event.target.value.length < 11){
+      setIsError(false);
+    } else {
+      setIsError(true);
+      setIsPhoneNumber(event.target.value);
+    }
+  }
+
+  const setName = (event) => {
+    setIsName(event.target.value);
+  }
+
   return (
     <div className="h-screen flex flex-col items-center bg-neutral-50">
-      <input type="text" placeholder="NAME" 
-        className="items-center"
-      ></input>
-      <input type="text" placeHolder="SURNAME"></input>
-      <input type="text" placeholder="PHONE NUMBER"></input>
-      <SubmitButton />
+      
+      <div className="flex flex-col gap-5 items-center cursor-default mt-32 bg-gray-200 py-5 px-10 shadow-gray-700 shadow-lg rounded-md">
+
+        <h1 className="menu-header">Client Information</h1>
+
+        <input type="text" placeholder="NAME" maxLength="20" className="input-purchase" onChange={setName}></input>
+        <input type="text" placeHolder="SURNAME" maxLength="20" className="input-purchase"></input>
+        <input type="text" placeholder="PHONE NUMBER" maxLength="15" className={isError ? "input-purchase" : "input-purchase bg-red-600"} onChange={setNumber}></input>
+        <SubmitButton number={isPhoneNumber} name={isName} active={isError} />
+        {/* <p className={isError ? "text-xs font-medium text-red-400 uppercase" : "hidden"}>invalid number</p> */}
+        
+      </div>
     </div>
   )
 }
