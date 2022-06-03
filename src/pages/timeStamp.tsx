49f6ae16bda6f000365543bc518d30a6c6955b23
SmallPeenwow <bowdencamryn@gmail.com>
2022-06-03 21:38:37 +0200

    TimeStamp done

--- src/pages/timeStamp.tsx
+++ src/pages/timeStamp.tsx
@@ -1,14 +1,17 @@
 import React from 'react'
+import { Countdown } from '../hooks/countdown';
 
 const timeStamp = () => {
+    const { days, hours, minutes, seconds } = Countdown();
+
   return (
-    <div className="h-full flex">
-        <div className="">
-            <input type="number" name="days"></input>
-            <input type="number" name="hours"></input>
-            <input type="number" name="min"></input>
-            <input type="number" name="second"></input>
-        </div>
+    <div className="h-screen flex md:flex-row items-center gap-20 justify-center bg-neutral-50">
+        
+        <p className="day-p-styling"><span className="span-styling">{days}</span>Days</p>
+        <p className="day-p-styling"><span className="span-styling">{hours}</span>Hours</p>
+        <p className="day-p-styling"><span className="span-styling">{minutes}</span>Minutes</p>
+        <p className="day-p-styling"><span className="span-styling">{seconds}</span>Seconds</p>
+
     </div>
   )
 }
