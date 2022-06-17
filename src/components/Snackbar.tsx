a30f47ddf6f655df321d4621f9140a46f391f80b
SmallPeenwow <bowdencamryn@gmail.com>
2022-06-17 14:26:23 +0200

    email and snackbar fixers done

--- src/components/Snackbar.tsx
+++ src/components/Snackbar.tsx
@@ -2,9 +2,9 @@ import React, { PureComponent } from 'react';
 
 export class Snackbar extends PureComponent<{ message: string; isActive: boolean; error: boolean; }> {
     render() {
-        const { isActive, message } = this.props;
+        const { isActive, message, error } = this.props;
         return (
-            <div className={isActive ? "snackbar-visible animate-fade-in" : "snackbar-invisible"}>
+            <div className={isActive ? error ? "snackbar-visible animate-fade-in text-lg bg-green-600" : "snackbar-visible animate-fade-in text-base bg-red-600/95" : "snackbar-invisible"}>
                 {message}
             </div>
         )
